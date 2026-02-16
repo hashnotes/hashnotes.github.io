f19714fcd7855d1728e0c41b47278ecc89debe93
DKormann <dominikormann@outlook.com>
2026-02-16 12:35:22 +0100

    counter cleanup

--- lib/scripts/playground.ts
+++ lib/scripts/playground.ts
@@ -1,33 +1,26 @@
 import { noteBody, note, publishView } from "./note-fn.ts";
 
 const remoteFn = noteBody(function () {
-  let count = store.get("count") || 0;
-  count += arg.dif;
-  store.set("count", count);
-  return count;
+  let c = store.get("count") || 0 as number;
+  c += arg.d;
+  store.set("count", c);
+  return c
 });
 
 await publishView(
   note(function () {
     return (upper: UPPER) => {
       let label = HTML.p("count: loading...");
-      let root: VDom;
-      let update = (dif: number) =>
-        remote(remoteFn, { dif }).then((c) => {
-          label.textContent = "count: " + c;
-          upper.update(root);
-        });
-      root = HTML.div(
-        HTML.h3("Store View 2"),
-        label,
-        HTML.button("increment", {
-          onclick: (e: DomEvent) => {
-            if (e.type !== "click") return;
-            update(1);
-          }
-        })
+      let inc = (d:number) => remote(remoteFn, {d}).then(c=>{
+        label.textContent = "count: " + c
+        upper.update(label)
+      })
+
+      let root = HTML.div(
+        HTML.h3("Store View"),
+        label, HTML.button("inc", {onclick:()=>inc(1)})
       );
-      update(1);
+      inc(0)
       return root;
     };
   }, { remoteFn })
